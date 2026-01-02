0f2f51df07e0011c0761c87a18ad5c0ead903a71
sude <223205048@kku.edu.tr>
2026-01-02 06:35:58 +0300

    backend

--- plants/forms.py
+++ plants/forms.py
@@ -1,9 +1,5 @@
 from django import forms
-<<<<<<< HEAD
 from .models import Plant, CareLog
-=======
-from .models import Plant
->>>>>>> 7160f4d21e2332bebb61782cb3b7a70d4d89e490
 
 class PlantForm(forms.ModelForm):
     class Meta:
@@ -14,7 +10,6 @@ class PlantForm(forms.ModelForm):
             'photo', 
             'location', 
             'irrigation_frequency'
-<<<<<<< HEAD
         ]
 
 class CareLogForm(forms.ModelForm):
@@ -31,6 +26,3 @@ class CareLogForm(forms.ModelForm):
                 attrs={'type': 'date'}
             )
         }
-=======
-        ]
->>>>>>> 7160f4d21e2332bebb61782cb3b7a70d4d89e490
